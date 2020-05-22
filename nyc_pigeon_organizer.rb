5c9a3e674172fe9bbb900b8844fb83062ef4a528
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-22 00:22:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,12 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   # identify pigeon names as top levels
-  
+  organized_pigeon = {}
+  data[:color].each do |key, value|
+    value.each do |name|
+      organized_pigeon[name] = {}
+      organized_pigeon.uniq
+    end
+  end
 end
 
